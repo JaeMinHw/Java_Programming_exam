af33df568b4b101f79eabcb95bf3941a6b53be17
JaeMinHw <95284552+JaeMinHw@users.noreply.github.com>
2022-04-18 14:10:15 +0900

    Update Login_pop.java

--- Login_pop.java
+++ Login_pop.java
@@ -1,10 +1,24 @@
 package java_programming_project;
 
-public class Login_pop {
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+public class Login_pop extends JFrame {
+
+	Login_pop() {
+		setTitle(""); // 가게 이름
+		setLayout(new BorderLayout(10, 10));
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(300, 150);
+		setVisible(true);
+
+	}
 
 	public void Check_login() {
 		// 아이디 비밀번호 값 받아와서 확인하기
-		System.out.println("로그인 버튼");
+
 	}
 
 }
