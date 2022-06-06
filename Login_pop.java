c29c893369f9a0dfc220e947ad8c6643c1f39f02
JaeMinHw <95284552+JaeMinHw@users.noreply.github.com>
2022-06-06 16:42:45 +0900

    Update Login_pop.java

--- Login_pop.java
+++ Login_pop.java
@@ -13,7 +13,10 @@ public class Login_pop extends JFrame {
 	RoundedButton store_manage = new RoundedButton("매장 관리");
 	RoundedButton order = new RoundedButton("주문");
 
+	public String next_id;
+
 	public void log_pop(String id) {
+		next_id = id;
 		database db;
 		try {
 			db = new database();
@@ -22,7 +25,6 @@ public class Login_pop extends JFrame {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-
 		setLayout(new BorderLayout(10, 10));
 		show_second();
 
@@ -44,7 +46,7 @@ public class Login_pop extends JFrame {
 		order.addActionListener(new manageButtonListener());
 	}
 
-	// 매장관리 버튼 누르면 반응하는 클래스
+	// 버튼 누르면 반응하는 클래스
 	class manageButtonListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
@@ -53,14 +55,14 @@ public class Login_pop extends JFrame {
 
 			if (log_comm.equals("매장 관리")) {
 				// 매장 관리를 누르면 메뉴 수정, 매출 등 관리 위주
-				//
-				System.out.println("매장 관리");
 				Manage_ mana = new Manage_();
-				mana.show_third();
+				mana.show_third(next_id);
 
 			} else if (log_comm.equals("주문")) {
 				// 주문을 누르면 pos UI 나오게
-				System.out.println("주문");
+				Order_ orde = new Order_();
+				orde.TestFrame(next_id);
+
 			}
 
 		}
